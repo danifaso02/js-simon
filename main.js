544efad832ab718a6d801ba73827f9839022481e
danifaso02 <danifaso02@gmail.com>
2022-11-04 14:24:08 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+"use: strict";
+
+// Visualizzare in pagina 5 numeri casuali da 1 a 100 senza duplicati.
+// Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
